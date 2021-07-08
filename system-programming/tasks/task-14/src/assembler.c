f871b2ad63abdfab0673e596edd8a480cad564b8
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-08 23:46:15 +0300

    feat: add file reader

--- system-programming/tasks/task-14/src/assembler.c
+++ system-programming/tasks/task-14/src/assembler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
@@ -12,7 +13,48 @@ int main(int argc, char **argv)
 
     for (i = 1; i < argc; ++i)
     {
-        printf("file[%d]: %s\n", i, argv[i]);
+        FILE *file;
+        char character;
+        char *extension;
+        char *ext = ".as";
+
+        printf("Read: %s\n\n", argv[i]);
+
+        extension = strrchr(argv[i], '.');
+
+        if (strcmp(extension, ext) != 0)
+        {
+            printf("The assembly file must be with .as extension.\n\n");
+            continue;
+        }
+
+        /* Open the source file with the ".c" extension (e.g. prnt.c). */
+        file = fopen(argv[i], "r");
+
+        /* If something is wrong then print the error message. */
+        if (file == NULL)
+        {
+            perror("Unable to open file");
+            printf("\n");
+            continue;
+        }
+
+        /* Print all the file content. */
+        while (1)
+        {
+            character = fgetc(file);
+
+            if (character == EOF)
+            {
+                break;
+            }
+
+            printf("%c", character);
+        }
+
+        printf("\n\n");
+
+        fclose(file);
     }
 
     return 0;
