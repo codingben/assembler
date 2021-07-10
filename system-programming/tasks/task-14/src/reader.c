4189395cf08f4e8b7ac6c2ee18fd6c86d5d62326
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-10 17:06:17 +0300

    docs: remove comments

--- system-programming/tasks/task-14/src/reader.c
+++ system-programming/tasks/task-14/src/reader.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
+#include "constants.h"
 #include "parser.h"
 
 void read_file(const char *filename)
 {
-    /* Open the source file with the ".c" extension (e.g. prnt.c). */
     FILE *file = fopen(filename, "r");
 
-    /* If something is wrong then print the error message. */
     if (file != NULL)
     {
-        /* Check syntax and then send to translator */
         parse_file(file, filename);
-
-        /* Close file after parsing */
         fclose(file);
     }
     else
     {
-        perror("Unable to open file");
+        perror(FAILED_OPEN_FILE);
     }
 }
