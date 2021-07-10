7d267ab2dacc58b1c3e96ec69936961da4bb276d
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-10 17:06:54 +0300

    feat: add constants

--- system-programming/tasks/task-14/src/constants.h
+++ system-programming/tasks/task-14/src/constants.h
@@ -1,7 +1,15 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+/* Assembler rules */
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
 #define ASSEMBLY_EXTENSION ".as"
 #define MAX_LINE_LENGTH 81 /* The last character is '\n' */
+
+/* Log messages */
+#define NO_FILE_TO_COMPILE "could not find a file to compile."
+#define NO_AS_EXTENSION "assembly file must be with .as extension."
+#define FAILED_OPEN_FILE "Unable to open file"
 
 #endif
