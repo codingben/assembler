0306eb91704b9c37757597087d3067595caecd64
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-15 20:31:42 +0300

    fix: remove '\n' from logs

--- system-programming/tasks/task-14/src/logger.h
+++ system-programming/tasks/task-14/src/logger.h
@@ -2,12 +2,12 @@
 #define LOGGER_H
 
 /* Log format is: sourcefile: message */
-#define LOG_FORMAT "%s: %s\n"
+#define LOG_FORMAT "%s: %s"
 
 /* Log format is: sourcefile:linenum: message */
-#define LOG_LINE_FORMAT "%s:%d: %s\n"
+#define LOG_LINE_FORMAT "%s:%d: %s"
 
 /* Log format is: sourcefile:linenum:column: message */
-#define LOG_LINE_COLUMN_FORMAT "%s:%d: %s\n"
+#define LOG_LINE_COLUMN_FORMAT "%s:%d: %s"
 
 #endif
