14aa0177aa6d5fb3a9d36ff7654bc4e2b95f8e03
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-23 22:02:58 +0300

    refactor: cleanup assembler

--- system-programming/tasks/task-14/src/assembler.c
+++ system-programming/tasks/task-14/src/assembler.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include "constants/logger.h"
 #include "constants/messages.h"
-#include "constants/rules.h"
 #include "utils/file.h"
+#include "scanner/analyzer.h"
 #include "parser/parser.h"
+
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
 
 int main(int argument_count, char **argument_vector)
 {
     int i;
-    const char *filename = argument_vector[0];
+    const char *file_name = argument_vector[0];
+    LinkedLine *linked_line;
 
     if (argument_count < 2)
     {
@@ -19,24 +23,39 @@ int main(int argument_count, char **argument_vector)
 
     for (i = 1; i < argument_count; ++i)
     {
-        filename = argument_vector[i];
+        file_name = argument_vector[i];
 
-        /* parse (read_file) */
-        /* if (parse) compile */
-        /* if (compile) build */
+        /* Steps:
+         * 1. if (validated) analyze (scanner)
+         * 2. if (analyzed) parse
+         * 3. if (parsed) compile
+         * 4. if (compiled) build 
+         */
 
-        if (validate_extension(filename) != 0)
+        if (validate_extension(file_name) != 0)
         {
-            printf(ERROR_FORMAT, filename, NO_AS_EXTENSION);
+            printf(ERROR_FORMAT, file_name, NO_AS_EXTENSION);
             printf("#---------------------------------------------------#\n");
             continue;
         }
 
-        /* Analyze syntax and then send to translator */
-        parse(filename);
+        /* Scanner analysis */
+        linked_line = analyze(file_name);
+
+        if (linked_line == NULL)
+        {
+            /* TODO: Add error message here */
+            continue;
+        }
+
+        /* Syntax parsing */
+        parse(file_name, linked_line);
+
+        /* Free saved lines */
+        delete_linked_line(linked_line);
 
         /* Add space for a new file */
-        printf("\n#---------------------------------------------------#\n");
+        printf("#---------------------------------------------------#\n");
     }
 
     return EXIT_SUCCESS;
