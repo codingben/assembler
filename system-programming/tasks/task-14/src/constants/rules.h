9e09b040d589259fe204254a1e8f7da01a57948e
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-23 22:00:18 +0300

    refactor: remove rules header

--- system-programming/tasks/task-14/src/constants/rules.h
+++
@@ -1,9 +0,0 @@
-#ifndef RULES_H
-#define RULES_H
-
-#define EXIT_FAILURE 1
-#define EXIT_SUCCESS 0
-#define ASSEMBLY_EXTENSION ".as"
-#define MAX_LINE_LENGTH 81 /* The last character is '\n' */
-
-#endif
