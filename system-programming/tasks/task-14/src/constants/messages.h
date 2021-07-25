7f852ad6817bab5dd90df3886a5bc580f20bb188
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-25 23:41:15 +0300

    fix: change messages

--- system-programming/tasks/task-14/src/constants/messages.h
+++ system-programming/tasks/task-14/src/constants/messages.h
@@ -2,7 +2,7 @@
 #define MESSAGES_H
 
 #define NO_FILE_TO_COMPILE "error: could not find an assembly file to compile.\n"
-#define NO_AS_EXTENSION "error: an assembly file must be with .as extension.\n"
-#define FAILED_OPEN_FILE "error: unable to open the assembly file." /* No need '\n' here because used in perror(...). */
+#define NO_AS_EXTENSION "an assembly file must be with .as extension.\n"
+#define FAILED_ANALYZE_FILE "unable to analyze the assembly file.\n" /* No need '\n' here because used in perror(...). */
 
 #endif
