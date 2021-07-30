50b07d45b0fb0017d4fc10289ff2f206ad1eba9c
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-30 21:43:55 +0300

    feat: add line validator

---
+++ system-programming/tasks/task-14/src/line/validator.c
@@ -0,0 +1,9 @@
+int is_comment_line(char value)
+{
+    return value == ';';
+}
+
+int is_empty_line(char value)
+{
+    return value == '\n';
+}
---
+++ system-programming/tasks/task-14/src/line/validator.h
@@ -0,0 +1,8 @@
+#ifndef VALIDATOR_H
+#define VALIDATOR_H
+
+int is_comment_line(char value);
+
+int is_empty_line(char value);
+
+#endif
