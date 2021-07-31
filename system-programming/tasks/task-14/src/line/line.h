26984c612aa646df0b8022bb86a50b317d3d301e
Ben Ukhanov <ben1zuk321@gmail.com>
2021-07-31 21:26:53 +0300

    feat: add label, command and directive

--- system-programming/tasks/task-14/src/line/line.h
+++ system-programming/tasks/task-14/src/line/line.h
@@ -3,6 +3,58 @@
 
 #define ASM_MAX_LINE_LENGTH 81 /* The last character is '\n' */
 #define ERROR_MAX_LINE_LENGTH 120
+#define MAX_LABEL_LENGTH 31
+#define MAX_COMMAND_LENGTH 5
+#define MAX_DIRECTIVE_LENGTH 10
+#define COMMANDS_SIZE 27
+#define DIRECTIVES_SIZE 6
+
+typedef struct
+{
+    char name[MAX_COMMAND_LENGTH];
+} Command;
+
+static const Command commands[COMMANDS_SIZE] = {
+    {"add"},
+    {"sub"},
+    {"and"},
+    {"or"},
+    {"nor"},
+    {"move"},
+    {"mvhi"},
+    {"mvlo"},
+    {"addi"},
+    {"subi"},
+    {"andi"},
+    {"ori"},
+    {"nori"},
+    {"bne"},
+    {"beq"},
+    {"blt"},
+    {"bgt"},
+    {"lb"},
+    {"sb"},
+    {"lw"},
+    {"sw"},
+    {"lh"},
+    {"sh"},
+    {"jmp"},
+    {"la"},
+    {"call"},
+    {"stop"}};
+
+typedef struct
+{
+    char name[MAX_DIRECTIVE_LENGTH];
+} Directive;
+
+static const Directive directives[DIRECTIVES_SIZE] = {
+    {".db"},
+    {".dw"},
+    {".dh"},
+    {".asciz"},
+    {".entry"},
+    {".extern"}};
 
 typedef enum
 {
@@ -20,6 +72,9 @@ typedef struct line
     char error_message[ERROR_MAX_LINE_LENGTH];
     int error_column;
     statement_type statement_type;
+    char label[MAX_LABEL_LENGTH];
+    char command[MAX_COMMAND_LENGTH];
+    char directive[MAX_DIRECTIVE_LENGTH];
     struct line *next;
     /* TODO: error list (message, column). but, one error is enough */
 } Line;
