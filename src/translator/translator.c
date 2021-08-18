46891460c729823b0403fdbff4cfab77e6aa05e8
Ben Ukhanov <ben1zuk321@gmail.com>
2021-08-18 23:10:54 +0300

    feat: add translator

---
+++ src/translator/translator.c
@@ -0,0 +1,8 @@
+char *translate(char *value)
+{
+    /* TODO: Where the translation will be saved? */
+    /* TODO: How outout files will be saved? */
+    /* TODO: How to indicate about entry/extern to create them? */
+    /* TODO: Validate all data in parser before sending to translator */
+    /* TODO: Translate to binary for output files creation (entry/extern also) */
+}
---
+++ src/translator/translator.h
@@ -0,0 +1,6 @@
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+
+char *translate(char *value);
+
+#endif
