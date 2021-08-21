0368560388566f20cf91fc08b73506c62e29fc70
Ben Ukhanov <ben1zuk321@gmail.com>
2021-08-21 19:45:48 +0300

    feat: add more command variables

--- src/line/line.h
+++ src/line/line.h
@@ -12,39 +12,49 @@
 #define REGISTER_SIZE 32
 #define MAX_OPERANDS_SIZE 80
 
+typedef enum
+{
+    R,
+    I,
+    J,
+} command_type;
+
 typedef struct
 {
     char name[MAX_COMMAND_LENGTH];
+    command_type command_type;
+    unsigned int funct;
+    unsigned int opcode;
 } Command;
 
 static const Command commands[COMMANDS_SIZE] = {
-    {"add"},
-    {"sub"},
-    {"and"},
-    {"or"},
-    {"nor"},
-    {"move"},
-    {"mvhi"},
-    {"mvlo"},
-    {"addi"},
-    {"subi"},
-    {"andi"},
-    {"ori"},
-    {"nori"},
-    {"bne"},
-    {"beq"},
-    {"blt"},
-    {"bgt"},
-    {"lb"},
-    {"sb"},
-    {"lw"},
-    {"sw"},
-    {"lh"},
-    {"sh"},
-    {"jmp"},
-    {"la"},
-    {"call"},
-    {"stop"}};
+    {"add", R, 1, 0},
+    {"sub", R, 2, 0},
+    {"and", R, 3, 0},
+    {"or", R, 4, 0},
+    {"nor", R, 5, 0},
+    {"move", R, 1, 1},
+    {"mvhi", R, 2, 1},
+    {"mvlo", R, 3, 1},
+    {"addi", I, 0, 10},
+    {"subi", I, 0, 11},
+    {"andi", I, 0, 12},
+    {"ori", I, 0, 13},
+    {"nori", I, 0, 14},
+    {"bne", I, 0, 15},
+    {"beq", I, 0, 16},
+    {"blt", I, 0, 17},
+    {"bgt", I, 0, 18},
+    {"lb", I, 0, 19},
+    {"sb", I, 0, 20},
+    {"lw", I, 0, 21},
+    {"sw", I, 0, 22},
+    {"lh", I, 0, 23},
+    {"sh", I, 0, 24},
+    {"jmp", J, 0, 30},
+    {"la", J, 0, 31},
+    {"call", J, 0, 32},
+    {"stop", J, 0, 63}};
 
 typedef struct
 {
