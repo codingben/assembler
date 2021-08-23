289e2fea44dd9d2d023d66cb0af79ced7d8256d2
Ben Ukhanov <ben1zuk321@gmail.com>
2021-08-23 22:24:10 +0300

    feat: add messages

--- src/constants/messages.h
+++ src/constants/messages.h
@@ -3,7 +3,8 @@
 
 #define NO_FILE_TO_COMPILE "error: could not find an assembly file to compile.\n"
 #define NO_AS_EXTENSION "an assembly file must be with .as extension.\n"
-#define FAILED_ANALYZE_FILE "unable to analyze the assembly file.\n" /* No need '\n' here because used in perror(...). */
+#define FAILED_ANALYZE_FILE "unable to analyze the assembly file.\n"
+#define FAILED_TRANSLATE_FILE "unable to translate the assembly file.\n"
 #define MAX_LENGTH_LABEL "label length has reached its limit.\n"
 #define MAX_EQUALS_COMMAND "label cannot be command.\n"
 #define MAX_EQUALS_DIRECTIVE "label cannot be directive.\n"
