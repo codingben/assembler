0044788e7e11e323621676692a17074a2d052a98
Ben Ukhanov <ben1zuk321@gmail.com>
2021-08-23 23:02:24 +0300

    refactor: main function

--- src/assembler.c
+++ src/assembler.c
@@ -10,6 +10,7 @@
 #include "scanner/analyzer.h"
 #include "parser/parser.h"
 #include "symbol/symbol.h"
+#include "translator/translator.h"
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
@@ -32,6 +33,7 @@ int main(int argument_count, char **argument_vector)
         LinkedSymbol *linked_symbol = NULL;
         LinkedLine *linked_line = NULL;
         int validated = FALSE;
+        int analyzed = FALSE;
         int parsed = FALSE;
         int translated = FALSE;
 
@@ -55,41 +57,42 @@ int main(int argument_count, char **argument_vector)
 
         printf("# Analyzing File: %s\n", file_name);
 
-        /* Reading the assembly source code. */
+        /* Read the assembly source code. */
         linked_line = analyze(file_name);
 
-        if (linked_line == NULL)
-        {
-            printf(ERROR_FORMAT, file_name, FAILED_ANALYZE_FILE);
-            continue;
-        }
-
-        /* Syntax parsing. */
+        /* Create symbol table. */
         linked_symbol = create_linked_symbol();
 
-        if (linked_symbol == NULL)
+        /* Check if analyzed and symbol table created. */
+        analyzed = linked_line != NULL && linked_symbol != NULL;
+
+        if (analyzed)
         {
-            printf(ERROR_FORMAT, file_name, FAILED_ANALYZE_FILE);
-            continue;
-        }
+            printf("# Parsing File: %s\n", file_name);
 
-        printf("# Parsing File: %s\n", file_name);
+            /* Parse the syntax. */
+            parsed = parse(file_name, linked_line, linked_symbol);
 
-        parsed = parse(file_name, linked_line, linked_symbol);
+            if (parsed)
+            {
+                printf("# Translating File: %s\n", file_name);
 
-        if (parsed)
-        {
-            /* TODO: compiled = compile(...) */
+                /* Translate the parsed syntax. */
+                translated = translate(linked_line, linked_symbol);
+
+                if (translated)
+                {
+                    printf("# Generate Output Files For: %s\n", file_name);
+                }
+                else
+                {
+                    printf(ERROR_FORMAT, file_name, FAILED_TRANSLATE_FILE);
+                }
+            }
         }
         else
         {
             printf(ERROR_FORMAT, file_name, FAILED_ANALYZE_FILE);
-            continue;
-        }
-
-        if (translated)
-        {
-            /* TODO: Build object file (and other files if needed) */
         }
 
         delete_linked_line(linked_line);
